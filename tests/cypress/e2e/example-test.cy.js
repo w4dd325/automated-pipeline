161354e417c46020864353f6d2eccd60382ea1f9
James Wadley <86320001+w4dd325@users.noreply.github.com>
2025-03-26 23:40:25 +0000

    Added a basic Cypress test

---
+++ tests/cypress/e2e/example-test.cy.js
@@ -0,0 +1,22 @@
+describe('My First Test', () => {
+    it('Page should have expected elements', () => {
+
+        cy.visit('http://localhost:8080/')
+
+        cy.get('[test-id="instruction-text"]')
+            .should('have.text', 'Click button to display datetime');
+
+        cy.get('[test-id="show-date-time-button"]')
+            .should('have.text', 'Show Date and Time')
+            .should('be.enabled')
+            .should('be.visible')
+            .click();
+
+        cy.get('[test-id="date-time-output"]')
+            .should('include.text', '2025');
+
+        cy.get('[test-id="example-text"]')
+            .should('have.text', 'Example text to change for demo/deployment purposes ... ');
+
+    })
+})
